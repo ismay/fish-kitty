7db338bdead82dbd6780b4e05eeedc7efbfdb483
ismay <7355199+ismay@users.noreply.github.com>
2022-04-15 14:24:53 +0200

    Fix with fish_indent

--- completions/theme.fish
+++ completions/theme.fish
@@ -1,16 +1,16 @@
 function _theme_complete -d "Complete the theme command"
-  # Only completes first argument
-  if ! __fish_is_first_arg >/dev/null
-    return
-  end
+    # Only completes first argument
+    if ! __fish_is_first_arg >/dev/null
+        return
+    end
 
-  # Path to search for theme conf files
-  set -l theme_folder (command realpath ~/.config/kitty/themes/)
+    # Path to search for theme conf files
+    set -l theme_folder (command realpath ~/.config/kitty/themes/)
 
-  for theme in $theme_folder/*.conf
-    set -l theme_name (basename $theme .conf)
-    echo $theme_name
-  end
+    for theme in $theme_folder/*.conf
+        set -l theme_name (basename $theme .conf)
+        echo $theme_name
+    end
 end
 
 complete -c theme -fk -a "(_theme_complete)"
--- functions/theme.fish
+++ functions/theme.fish
@@ -1,8 +1,8 @@
 function theme -d "Change kitty theme" -a theme_name
-  # Do nothing if there is no theme specified
-  if [ -z "$theme_name" ]
-    return
-  end
+    # Do nothing if there is no theme specified
+    if [ -z "$theme_name" ]
+        return
+    end
 
-  kitty +kitten themes --reload-in=all $theme_name
+    kitty +kitten themes --reload-in=all $theme_name
 end
